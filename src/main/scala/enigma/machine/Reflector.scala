70737082d9e4f3b8cf9de096f88d1076f33d1ddf
Codeatron5000 <cdstarling@outlook.com>
2020-02-13 22:53:27 +0000

    Refactor Reflector

--- src/main/scala/enigma/machine/Reflector.scala
+++ src/main/scala/enigma/machine/Reflector.scala
@@ -7,25 +7,28 @@ sealed abstract class ReflectorWiring(val wiring: String)
  * another.
  */
 sealed trait Reflector {
-  /**
-   * The string of letters that are mapped to the alphabet.
-   */
-  val wiring: String
+    /**
+     * The string of letters that are mapped to the alphabet.
+     */
+    val wiring: String
 
-  /**
-   * Build the char map from the alphabet to the scrambled char.
-   */
-  val sequence: Map[Char, Char] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray
-      .zip(wiring.toCharArray).toMap
+    /**
+     * Build the char map from the alphabet to the scrambled char.
+     */
+    val sequence: Map[Char, Char] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray
+        .zip(wiring.toCharArray).toMap
 
-  /**
-   * Encode a char through the reflector (there is not need for a decode method
-   * as the reflector encodes the same char both ways.
-   */
-  def encode(letter: Char): Char = sequence(letter)
+    /**
+     * Encode a char through the reflector (there is not need for a decode method
+     * as the reflector encodes the same char both ways.
+     */
+    def encode(letter: Char): Char = sequence(letter)
 }
 
 object Reflector {
-  case object B extends ReflectorWiring("YRUHQSLDPXNGOKMIEBFZCWVJAT") with Reflector
-  case object C extends ReflectorWiring("FVPJIAOYEDRZXWGCTKUQSBNMHL") with Reflector
+
+    case object B extends ReflectorWiring("YRUHQSLDPXNGOKMIEBFZCWVJAT") with Reflector
+
+    case object C extends ReflectorWiring("FVPJIAOYEDRZXWGCTKUQSBNMHL") with Reflector
+
 }
